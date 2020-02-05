77f61e81458cb3b963ad44148fad21906bd3bafb
Lasse <lasseh0310@gmail.com>
2020-02-05 13:44:23 +0100

    add dependency distance

---
+++ dependency_distance.py
@@ -0,0 +1,120 @@
+import os
+import stanfordnlp
+import numpy as np
+
+class Dep_distance():
+    def __init__(self, text, lang, snlp_path):
+        self.text = text
+        self.lang = lang
+        if snlp_path is None:
+            self.snlp_path = os.getcwd() + 'snlp_resources'
+        else:
+            self.snlp_path = snlp_path
+
+        self.dep_dist()
+
+    def dl_missing_langs_snlp(self):
+        """
+        Downloads any missing languages from Stanford NLP resources
+        """
+        import stanfordnlp
+        
+        if not os.path.exists(self.snlp_path):
+            os.makedirs(self.snlp_path)
+
+        dl_langs = [folder[:2] for folder in os.listdir(self.snlp_path)]
+
+        if self.lang not in dl_langs:
+            stanfordnlp.download(self.lang, resource_dir=self.snlp_path, force = True)
+
+    def dep_dist(self):
+        """
+        Calculates dependency distance of the text on sentence level
+
+        """
+        #Check if snlp language resources are installed, otherwise download them
+        try: 
+            self.dl_missing_langs_snlp()
+            # If the specified language is not in SNLP, throw error and stop the function
+        except ValueError:
+            ValueError(f"Language '{self.lang}' does not exist in stanford NLP. Try specifying another language")
+                
+        if 's_nlp' not in globals():
+            global s_nlp
+            s_nlp = stanfordnlp.Pipeline(lang = self.lang, models_dir = self.snlp_path, 
+                    processors="tokenize,pos,depparse")
+
+        # Calculating DD for each text
+        list_sent_dep_dist = []
+        list_sent_prop_adj_rel = []
+        for txt in self.text:
+            doc = s_nlp(txt)
+
+            parsed = [(sent_n, word.index, word.governor, word.dependency_relation) for sent_n, sent in enumerate(doc.sentences) for word in sent.words]
+
+            n_sentences = set([token[0] for token in parsed])
+
+            sent_dep_dist = []
+            sent_prop_adjacent_relation = []
+            for sentence in n_sentences:
+                dep_dists = []
+                adj_rel = 0
+                tokens = 0
+                for sent_n, idx, governor, dep_relation in parsed:
+                    if sent_n == sentence:
+                        if dep_relation != 'root':
+                            dist = abs(governor - int(idx))
+                            dep_dists.append(dist)
+                            if dist == 1:
+                                adj_rel += 1
+                        else:
+                            dep_dists.append(0)
+                        tokens += 1
+                sent_dep_dist.append(sum(dep_dists) / len(dep_dists))
+                prop_adjacent =  adj_rel / tokens
+
+                sent_prop_adjacent_relation.append(prop_adjacent)
+            
+            list_sent_dep_dist.append(sent_dep_dist)
+            list_sent_prop_adj_rel.append(sent_prop_adjacent_relation)
+
+        self.sent_dep_dist = list_sent_dep_dist
+        self.sent_prop_adjacent_relation = list_sent_prop_adj_rel
+
+    def mean_dep_dist(self):
+        """
+        Calculates mean dependency distance
+        """
+        mdd = [np.mean(text) for text in self.sent_dep_dist]
+
+        return mdd
+
+    def std_dep_dist(self):
+        """
+        Calculates the standard deviation of the sentence level mean dependency distance
+        """
+        std_dd = [np.std(text) for text in self.sent_dep_dist]
+
+        return std_dd
+    
+    def proportion_adjacent_dep(self):
+        """
+        Calculates the average proportion of adjacent dependency relations on sentence level
+        """
+        prop_adj_dep = [np.mean(prop_adj) for prop_adj in self.sent_prop_adjacent_relation]
+        
+        return prop_adj_dep
+
+    def std_proportion_adjacent_dep(self):
+        """
+        Calculates the standard deviation of the proportion of adjacent dependency relations on sentence level
+        """
+        std_prop_adj_dep = [np.std(prop_adj) for prop_adj in self.sent_prop_adjacent_relation]
+
+
+#texts = ["Her er et par sætninger på dansk. Der er bare to. Måske er der tre, men de er ret korte",
+#        "Endnu en lille hyggesætning her, der dog er noget længere og med en lang referent. Det må jeg nok sige, sikke dog en lang sætning."]
+
+#text = ["Bare en enkelt sætning for lige at teste"]
+
+#dep = Dep_distance(texts, 'da', snlp_path)
--- textdescriptives.py
+++ textdescriptives.py
@@ -1,10 +1,11 @@
 from calculators import Calculators
 from readability import Readability
+from dependency_distance import Dep_distance
 import pandas as pd
 
 
 class Textdescriptives():
-    def __init__(self, texts, lang = 'da', category = 'all', measures = 'all'):
+    def __init__(self, texts, lang = 'da', category = 'all', measures = 'all', snlp_path = None):
         """
         texts: str/list/pd.Series containing text
         lang: str with the language code
@@ -19,6 +20,7 @@ class Textdescriptives():
             texts = [texts]
         self.df = pd.DataFrame(texts, columns = ['Text'])
         self.lang = lang
+        self.snlp_path = snlp_path
 
 
         valid_categories = ['all', 'basic', 'readability', 'entropy', 'sentiment', 'etymology']
@@ -29,6 +31,7 @@ class Textdescriptives():
             self.basic()
             self.readability()
             self.etymology()
+            self.dependency_distance()
 
         if 'basic' in category:
             self.basic(measures = measures)
@@ -36,6 +39,8 @@ class Textdescriptives():
             self.readability(measures = measures)
         if 'etymology' in category:
             self.etymology()
+        if 'dep_distance' in category:
+            self.dependency_distance()
 
 
     def basic(self, measures = 'all'):
@@ -107,6 +112,22 @@ class Textdescriptives():
             self.df['Latinate/Germanic'] = self.df['Latinate'] / self.df['Germanic']
         except KeyError:
             self.df['Latinate/Germanic'] = 'No Latinate'
+        
+    def dependency_distance(self):
+        """
+        Calculates mean dependency distance (MDD) of each text, following http://www.lingviko.net/jcs.pdf
+        Dependency distance (DD) = abs(governor - dependent)
+        As such, adjacent words have a DD of 1. Roots are defined as have a DD of 0
+        MDD is calculated on sentence level, ie. MDD is the mean of the average dependency distance pr sentence.
+        Mean and standard deviation of the proportion of adjacent dependency relations pr sentence is further calculated
+        """
+        dep = Dep_distance(self.texts, self.lang, self.snlp_path)
+
+        self.df['mean_dependency_distance'] = dep.mean_dep_dist()
+        self.df['std_dependency_distance'] = dep.std_dep_dist()
+        self.df['mean_prop_adjacent_dependency_relation'] = dep.proportion_adjacent_dep()
+        self.df['std_prop_adjacent_dependency_relation'] = dep.std_proportion_adjacent_dep()
+ 
     
     def entropy(self):
         pass
@@ -118,20 +139,28 @@ class Textdescriptives():
         return self.df
 
 
+
+dr_test = """
+Familiemedlemmer til de sigtede var mødt op i retten, og den ene af de sigtede fik også lov til at hilse på dem og give dem knus. Samtalen skulle dog foregå på dansk, fik han at vide.
+
+De tre sigtede er alle tilknyttet bestyrelsen i den danske gren af organisationen ASMLA, der repræsenterer et iransk-arabisk mindretal i den iranske provins Khuzestan.
+
+Organisationen arbejder for, at provinsen kan blive en selvstændig arabisk stat.
+
+I retten havde de to af de tre sigtede brug for en tolk, der hjalp dem med at oversætte den danske sigtelse.
+"""
+
+
+
 test = ['Det her er en lang dansk test. Gad vide om den virker efter hensigten',
         'Også lige en til her, ja tak ja tak. Vi tester bare lige om funktionen virker efter hensigten',
         "Meget formelt sprogbrug, information processering computer, hvorfor finder den ikke nogen ord"]
 
-test = "Meget formelt sprogbrug, information processering computer, hvorfor finder den ikke nogen ord" 
+snlp_path = '/home/au554730/Desktop/CHCAA/DeepAnon/stanfordnlp_resources'
 
-test = ['Not so much in this text, it is also short',
-'This is a test in English where I use quite posh words']
 t = Textdescriptives(test, 'da', 'basic', ['avg_word_length'])
 Textdescriptives(test, 'en', 'etymology').df
 t.basic('all')
 t.basic(['avg_word_length', 'median_word_length', 'fisse'])
 t.readability(['gunning_fog'])
 t.df
-sents = 'teste sdf. sd sd sd. d'
-sentences = tokenize.sent_tokenize(sents)
-
