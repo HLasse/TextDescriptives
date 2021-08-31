555f0915121ee63176f4423eda8819e048ffcf46
HLasse <lasseh0310@gmail.com>
2021-08-31 14:47:20 +0200

    suggested changes

--- textdescriptives/components/pos_stats.py
+++ textdescriptives/components/pos_stats.py
@@ -22,30 +22,12 @@ class POSStatistics:
 
     def __init__(self, nlp: Language):
         """Initialise components"""
-
-        extensions = [
-            "pos_proportions",
-        ]
-        ext_funs = [
-            self.pos_proportions,
-        ]
-        
-        # Unsure about how much of the below tooling code to keep; depends on whether we'll extend pos_stats in the future
-        for ext, fun in zip(extensions, ext_funs): 
-            if ext not in ["_n_sentences", "sentence_length", "syllables"]:
-                if not Span.has_extension(ext):
-                    Span.set_extension(ext, getter=fun)
-            if not Doc.has_extension(ext):
-                Doc.set_extension(ext, getter=fun)
-
-        if not Doc.has_extension("_filtered_tokens"):
-            Doc.set_extension("_filtered_tokens", default=[])
-        if not Span.has_extension("_filtered_tokens"):
-            Span.set_extension("_filtered_tokens", getter=filtered_tokens)
+        if not Doc.has_extension("pos_proportions"):
+            Doc.set_extension("pos_proportions", getter=self.pos_proportions)
+ 
 
     def __call__(self, doc):
         """Run the pipeline component"""
-        doc._._filtered_tokens = filtered_tokens(doc)
         return doc
 
     def pos_proportions(self, doc: Doc) -> dict:
