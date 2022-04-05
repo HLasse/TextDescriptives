74650ba2d6c67f97ab5c27f3ccb9b5ac12489a8c
Lasse <lasseh0310@gmail.com>
2022-04-05 15:13:28 +0200

    update: more wiggle room for pos tests

--- textdescriptives/tests/test_pos_stats.py
+++ textdescriptives/tests/test_pos_stats.py
@@ -3,22 +3,39 @@ from spacy.lang.en import English
 import pytest
 from textdescriptives.components import POSStatistics
 
+
 @pytest.fixture(scope="function")
 def nlp():
-    nlp = spacy.load("en_core_web_sm", disable=('ner', 'textcat'))
+    nlp = spacy.load("en_core_web_sm", disable=("ner", "textcat"))
     nlp.add_pipe("pos_stats")
 
     return nlp
 
+
 def test_pos_integrations(nlp):
     assert "pos_stats" == nlp.pipe_names[-1]
+
 
 def test_pos_proportions_doc(nlp):
     doc = nlp(
         "Here is the first sentence. It was pretty short. Let's make another one that's slightly longer and more complex."
     )
 
-    assert doc._.pos_proportions == pytest.approx({'pos_prop_ADV': 0.1666, 'pos_prop_AUX': 0.0833, 'pos_prop_DET': 0.125, 'pos_prop_ADJ': 0.1666, 'pos_prop_NOUN': 0.0833, 'pos_prop_PUNCT': 0.125, 'pos_prop_PRON': 0.0833, 'pos_prop_VERB': 0.125, 'pos_prop_CCONJ': 0.0416}, rel=1e-2)
+    assert doc._.pos_proportions == pytest.approx(
+        {
+            "pos_prop_ADV": 0.1666,
+            "pos_prop_AUX": 0.0833,
+            "pos_prop_DET": 0.125,
+            "pos_prop_ADJ": 0.1666,
+            "pos_prop_NOUN": 0.0833,
+            "pos_prop_PUNCT": 0.125,
+            "pos_prop_PRON": 0.0833,
+            "pos_prop_VERB": 0.125,
+            "pos_prop_CCONJ": 0.0416,
+        },
+        rel=0.05,
+    )
+
 
 def test_pos_proportions_span(nlp):
     doc = nlp(
@@ -27,5 +44,17 @@ def test_pos_proportions_span(nlp):
 
     span = doc[0:]
 
-    assert doc._.pos_proportions == pytest.approx({'pos_prop_ADV': 0.1666, 'pos_prop_AUX': 0.0833, 'pos_prop_DET': 0.125, 'pos_prop_ADJ': 0.1666, 'pos_prop_NOUN': 0.0833, 'pos_prop_PUNCT': 0.125, 'pos_prop_PRON': 0.0833, 'pos_prop_VERB': 0.125, 'pos_prop_CCONJ': 0.0416}, rel=1e-2)
-    
+    assert doc._.pos_proportions == pytest.approx(
+        {
+            "pos_prop_ADV": 0.1666,
+            "pos_prop_AUX": 0.0833,
+            "pos_prop_DET": 0.125,
+            "pos_prop_ADJ": 0.1666,
+            "pos_prop_NOUN": 0.0833,
+            "pos_prop_PUNCT": 0.125,
+            "pos_prop_PRON": 0.0833,
+            "pos_prop_VERB": 0.125,
+            "pos_prop_CCONJ": 0.0416,
+        },
+        rel=0.05,
+    )
