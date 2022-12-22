ec01f0a677894cbe3786e554536329bc7e602459
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2022-12-22 11:55:57 +0100

    fix: fixed missing imports

--- textdescriptives/__init__.py
+++ textdescriptives/__init__.py
@@ -1,2 +1,16 @@
 from .about import __title__, __version__  # noqa: F401
+from .components import (
+    DependencyDistance,  # noqa: F401
+    DescriptiveStatistics,  # noqa: F401
+    POSProportions,  # noqa: F401
+    Quality,  # noqa: F401
+    Readability,  # noqa: F401
+)
+from .dataframe_extract import (
+    dependency_cols,  # noqa: F401
+    descriptive_stats_cols,  # noqa: F401
+    extract_df,  # noqa: F401
+    extract_dict,  # noqa: F401
+    readability_cols,  # noqa: F401
+)
 from .load_components import TextDescriptives  # noqa: F401
