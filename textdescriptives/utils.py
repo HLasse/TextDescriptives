f42283d967c4de706149ea3148a5d5828ced98e7
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2022-12-22 16:16:31 +0100

    chore: moved spam.csv back into data

--- textdescriptives/utils.py
+++ textdescriptives/utils.py
@@ -38,7 +38,7 @@ def get_valid_metrics() -> set:
 def load_sms_data():
     """Load the sms dataset
     https://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection."""
-    path = Path(__file__).parent / "tests" / "data" / "spam.csv"
+    path = Path(__file__).parent / "data" / "spam.csv"
     spam_data = pd.read_csv(path, encoding="latin-1")
     spam_data.dropna(how="any", inplace=True, axis=1)
     spam_data.columns = ["label", "message"]
