7452abad3f044e1baabe03d12b1f0606178d0f99
Lasse <lasseh0310@gmail.com>
2023-01-02 10:01:05 +0100

    Update README Fixes #119

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,9 @@ autodoc_pydantic_model_show_json = False
 autodoc_pydantic_model_show_config = False
 autodoc_pydantic_model_show_field_summary = False
 
+# always fail CI pipeline when nb cannot be executed
+nb_execution_raise_on_error = True
+
 
 # -- Options for HTML output -------------------------------------------------
 
