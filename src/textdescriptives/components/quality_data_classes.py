ce91e568fe0926593de4b43fde8108f8b7b1da15
Lasse <lasseh0310@gmail.com>
2024-02-06 18:18:21 +0100

    feat: bump pydantic version to 2.0

--- src/textdescriptives/components/quality_data_classes.py
+++ src/textdescriptives/components/quality_data_classes.py
@@ -2,7 +2,7 @@
 
 from typing import Any, Dict, Optional, Tuple, Union
 
-from pydantic import BaseModel, Extra, Field
+from pydantic import ConfigDict, BaseModel, Field
 
 Interval = Tuple[Optional[float], Optional[float]]
 
@@ -20,9 +20,7 @@ class ThresholdsOutput(BaseModel):
         >>> t_out.passed
         True
     """
-
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
     threshold: Union[Interval, bool, None]
     value: Union[float, None]
@@ -60,9 +58,7 @@ class ThresholdsOutput(BaseModel):
 
 class QualityThresholds(BaseModel):
     """Thresholds for quality metrics."""
-
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
     n_stop_words: Interval = Field(
         (2, None),
@@ -163,9 +159,7 @@ class QualityThresholds(BaseModel):
 
 class QualityOutput(BaseModel):
     """The output of the quality function."""
-
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
     n_stop_words: ThresholdsOutput = Field(
         ...,
