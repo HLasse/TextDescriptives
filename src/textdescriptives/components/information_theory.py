9530103618c40d4f5cf1db4a6c0b9d9daccf1062
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-12-15 15:17:54 -0800

    fix: removed warning when dividing by 0

--- src/textdescriptives/components/information_theory.py
+++ src/textdescriptives/components/information_theory.py
@@ -64,7 +64,10 @@ def per_word_perplexity_getter(doc: Union[Doc, Span]) -> float:
     else:
         perplexity = perplexity_getter(doc)
 
-    return perplexity / len(doc)
+    len_doc = len(doc)
+    if len_doc:
+        return perplexity / len(doc)
+    return np.nan
 
 
 def set_docspan_extension(
